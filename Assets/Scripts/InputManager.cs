87096d807823e77ea6ae1e9008f7bdb9520bec83
Patryk Urban <p.urban@kainos.com>
2017-06-27 00:06:22 +0200

    Push'em

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public MainClicker mainClicker;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		HandleMouse();
+	}
+
+    private void HandleMouse()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null)
+            {
+                if (hit.collider.tag == "MainClicker")
+                {
+                    Debug.Log("click");
+                    mainClicker.OnMouseClicked();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MainClicker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainClicker : MonoBehaviour
+{
+    public float ShakeSpeed;
+    public Animator Animator;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+	{
+	    
+	}
+
+    public void ShakeIt()
+    {
+        var pos = transform.position;
+        pos.x = Mathf.Sin(Time.time * ShakeSpeed);
+
+        transform.position = pos;
+    }
+
+    public void OnMouseClicked()
+    {
+        Animator.SetTrigger("Active");
+    }
+}
